77fa9a34f835ae2b4c4b01a8646b73f42261e8d4
shradercinc <82776089+shradercinc@users.noreply.github.com>
2023-09-13 11:33:03 -0400

    Added new selection and object generation

--- Burn It Down/Assets/Scripts/GridManager.cs
+++ Burn It Down/Assets/Scripts/GridManager.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
+    [Header("Grid Settings")]
     [SerializeField] Vector2Int GridSize = new Vector2Int(10,10);
     private static int[,] _Grid;
     [SerializeField] float tileSize = 2;
     [SerializeField] GameObject genericTile;
     [SerializeField] float baseTileLayer = 0;
     public Vector2Int selectTile = new Vector2Int(-1,-1);
+    public List<GameObject> gridList = new List<GameObject>();
+    public List<FloorTile> FloorList = new List<FloorTile>();
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +26,13 @@ public class GridManager : MonoBehaviour
                 FloorTile curFloorScript = curTile.GetComponent<FloorTile>();
                 curFloorScript.gridPosition = new Vector2Int(i, j);
                 curFloorScript.manager = this;
+                gridList.Add(curTile);
+                FloorList.Add(curFloorScript);
             }
+        }
+        for (int i = 0; i < gridList.Count; i++)
+        {
+           if() 
         }
     }
 
