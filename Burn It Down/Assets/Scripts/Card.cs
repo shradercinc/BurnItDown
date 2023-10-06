4bf8f254bd13031bcd6f0d425679bdd0859cba1a
DZ-tt2195 <tt2195@nyu.edu>
2023-10-06 18:27:20 -0400

    moved code around

--- Burn It Down/Assets/Scripts/Card.cs
+++ Burn It Down/Assets/Scripts/Card.cs
@@ -13,16 +13,18 @@ public class StringAndMethod
 
     public StringAndMethod(Card card)
     {
-        dictionary["DrawCards"] = card.DrawCards();
-        dictionary["ChangeHP"] = card.ChangeHealth();
-        dictionary["ChangeEP"] = card.ChangeEnergy();
-        dictionary["ChangeMP"] = card.ChangeMovement();
+        dictionary["DRAWCARDS"] = card.DrawCards();
+        dictionary["CHANGEHP"] = card.ChangeHealth();
+        dictionary["CHANGEEP"] = card.ChangeEnergy();
+        dictionary["CHANGEMP"] = card.ChangeMovement();
     }
 
 }
 
 public class Card : MonoBehaviour, IPointerClickHandler
 {
+#region Card Stats
+
     [ReadOnly] public Image image;
     [ReadOnly] public SendChoice choiceScript;
 
@@ -32,29 +34,33 @@ public class Card : MonoBehaviour, IPointerClickHandler
     [ReadOnly] public CardType typeTwo;
     [ReadOnly] public bool violent;
 
-    [ReadOnly] public int changeInHP;
-    [ReadOnly] public int changeInMP;
-    [ReadOnly] public int changeInEP;
-    [ReadOnly] public int changeInDraw;
+    [ReadOnly] int changeInHP;
+    [ReadOnly] int changeInMP;
+    [ReadOnly] int changeInEP;
+    [ReadOnly] int changeInDraw;
 
-    [ReadOnly] public int stunDuration;
-    [ReadOnly] public int range;
-    [ReadOnly] public int areaOfEffect;
-    [ReadOnly] public int delay;
-    [ReadOnly] public int changeInWall;
-    [ReadOnly] public int burnDuration;
-    [ReadOnly] public int distractionIntensity;
+    [ReadOnly] int stunDuration;
+    [ReadOnly] int range;
+    [ReadOnly] int areaOfEffect;
+    [ReadOnly] int delay;
+    [ReadOnly] int changeInWall;
+    [ReadOnly] int burnDuration;
+    [ReadOnly] int distractionIntensity;
 
     public enum CanPlayCondition { None, Guard, Wall, Occupied };
-    [ReadOnly] public CanPlayCondition selectCondition;
-    [ReadOnly] public List<IEnumerator> effectsInorder = new List<IEnumerator>();
-    [ReadOnly] public string nextTurnAbility;
+    [ReadOnly] CanPlayCondition selectCondition;
+    [ReadOnly] List<IEnumerator> effectsInorder = new List<IEnumerator>();
+    [ReadOnly] List<IEnumerator> nextTurnEffectsInOrder = new List<IEnumerator>();
 
     [ReadOnly] public TMP_Text textName;
     [ReadOnly] public TMP_Text textCost;
     [ReadOnly] public TMP_Text textDescr;
 
     [ReadOnly] PlayerEntity currentPlayer;
+
+#endregion
+
+#region Setup
 
     private void Awake()
     {
@@ -79,6 +85,86 @@ public class Card : MonoBehaviour, IPointerClickHandler
             RightClick.instance.ChangeCard(this);
         }
     }
+
+    public void CardSetup(CardData data)
+    {
+        name = data.name;
+        textName.text = data.name;
+        textDescr.text = data.desc;
+
+        typeOne = ConvertToType(data.cat1);
+        typeTwo = ConvertToType(data.cat2);
+
+        energyCost = data.epCost;
+        textCost.text = $"{data.epCost} Energy";
+        violent = data.isViolent;
+
+        changeInHP = data.chHP;
+        changeInMP = data.chMP;
+        changeInEP = data.chEP;
+        changeInDraw = data.draw;
+
+        stunDuration = data.stun;
+        range = data.range;
+        areaOfEffect = data.aoe;
+        delay = data.delay;
+        changeInWall = data.wHP;
+
+        burnDuration = data.burn;
+        distractionIntensity = data.intn;
+
+        selectCondition = ConvertToCondition(data.select);
+        AddMethodsToList(data.action, effectsInorder);
+        AddMethodsToList(data.action, nextTurnEffectsInOrder);
+    }
+
+    CardType ConvertToType(string type)
+    {
+        return type switch
+        {
+            "draw" => CardType.Draw,
+            "atk" => CardType.Attack,
+            "dist" => CardType.Distraction,
+            "eng" => CardType.Energy,
+            "mvmt" => CardType.Movement,
+            "misc" => CardType.Misc,
+            _ => CardType.None,
+        };
+    }
+
+    CanPlayCondition ConvertToCondition(string condition)
+    {
+        return condition switch
+        {
+            "isGuard" => CanPlayCondition.Guard,
+            "isWall" => CanPlayCondition.Wall,
+            "isOccupied" => CanPlayCondition.Occupied,
+            _ => CanPlayCondition.None,
+        };
+    }
+
+    void AddMethodsToList(string divide, List<IEnumerator> list)
+    {
+        StringAndMethod dic = new StringAndMethod(this);
+        divide = divide.Replace(" ", "");
+        divide = divide.ToUpper();
+        string[] methodsInStrings = divide.Split('/');
+
+        for (int k = 0; k < methodsInStrings.Length; k++)
+        {
+            if (methodsInStrings[k] != "" || methodsInStrings[k] != "NONE")
+            {
+                if (dic.dictionary.TryGetValue(methodsInStrings[k], out IEnumerator method))
+                    list.Add(method);
+                else
+                    Debug.LogError($"\"{methodsInStrings[k]}\" isn't a method");
+            }
+        }
+    }
+
+#endregion
+
+#region Play Condition
 
     public bool CanPlay(PlayerEntity player)
     {
@@ -120,6 +206,10 @@ public class Card : MonoBehaviour, IPointerClickHandler
         return false;
     }
 
+    #endregion
+
+#region Play Effect
+
     public IEnumerator OnPlayEffect()
     {
         for (int i = 0; i < effectsInorder.Count; i++)
@@ -130,6 +220,58 @@ public class Card : MonoBehaviour, IPointerClickHandler
     {
         NewManager.instance.DrawCards(changeInDraw);
         yield return null;
+    }
+
+    public Card FindCardType(CardType type)
+    {
+        List<Card> invalidCards = new List<Card>();
+        Card foundCard = null;
+        while (foundCard == null)
+        {
+            Card nextCard = NewManager.instance.GetTopCard();
+            if (NewManager.instance.GetTopCard() == null)
+            {
+                break;
+            }
+            else if (nextCard.typeOne == type || nextCard.typeTwo == type)
+            {
+                foundCard = nextCard;
+            }
+            else
+            {
+                invalidCards.Add(nextCard);
+                nextCard.transform.SetParent(null);
+            }
+        }
+        for (int i = 0; i < invalidCards.Count; i++)
+            NewManager.instance.DiscardCard(invalidCards[i]);
+        return foundCard;
+    }
+
+    public Card FindCardCost(int cost)
+    {
+        List<Card> invalidCards = new List<Card>();
+        Card foundCard = null;
+        while (foundCard == null)
+        {
+            Card nextCard = NewManager.instance.GetTopCard();
+            if (NewManager.instance.GetTopCard() == null)
+            {
+                break;
+            }
+            else if (nextCard.energyCost == cost)
+            {
+                foundCard = nextCard;
+            }
+            else
+            {
+                invalidCards.Add(nextCard);
+                nextCard.transform.SetParent(null);
+            }
+        }
+        for (int i = 0; i < invalidCards.Count; i++)
+            NewManager.instance.DiscardCard(invalidCards[i]);
+        return foundCard;
     }
 
     public IEnumerator ChangeHealth()
@@ -149,4 +291,6 @@ public class Card : MonoBehaviour, IPointerClickHandler
         NewManager.instance.ChangeMovement(changeInMP);
         yield return null;
     }
+
+#endregion
 }
--- Burn It Down/Assets/Scripts/TSV Readers/CardReaderTest.cs
+++ Burn It Down/Assets/Scripts/TSV Readers/CardReaderTest.cs
@@ -26,75 +26,8 @@ public class CardReaderTest : MonoBehaviour
             {
                 Card nextCopy = Instantiate(cardPrefab, canvas);
                 nextCopy.transform.localPosition = new Vector3(10000, 10000);
-
-                nextCopy.name = data[i].name;
-                nextCopy.textName.text = data[i].name;
-                nextCopy.textDescr.text = data[i].desc;
-
-                nextCopy.typeOne = ConvertToType(data[i].cat1);
-                nextCopy.typeTwo = ConvertToType(data[i].cat2);
-
-                nextCopy.energyCost = data[i].epCost;
-                nextCopy.textCost.text = $"{data[i].epCost} Energy";
-                nextCopy.violent = data[i].isViolent;
-
-                nextCopy.changeInHP = data[i].chHP;
-                nextCopy.changeInMP = data[i].chMP;
-                nextCopy.changeInEP = data[i].chEP;
-                nextCopy.changeInDraw = data[i].draw;
-
-                nextCopy.stunDuration = data[i].stun;
-                nextCopy.range = data[i].range;
-                nextCopy.areaOfEffect = data[i].aoe;
-                nextCopy.delay = data[i].delay;
-                nextCopy.changeInWall = data[i].wHP;
-
-                nextCopy.burnDuration = data[i].burn;
-                nextCopy.distractionIntensity = data[i].intn;
-
-                nextCopy.selectCondition = ConvertToCondition(data[i].select);
-                AddMethodsToList(nextCopy, data[i].action, nextCopy.effectsInorder);
-                nextCopy.nextTurnAbility = data[i].nextAct;
+                nextCopy.CardSetup(data[i]);
             }
-        }
-    }
-
-    Card.CardType ConvertToType(string type)
-    {
-        return type switch
-        {
-            "draw" => Card.CardType.Draw,
-            "atk" => Card.CardType.Attack,
-            "dist" => Card.CardType.Distraction,
-            "eng" => Card.CardType.Energy,
-            "mvmt" => Card.CardType.Movement,
-            "misc" => Card.CardType.Misc,
-            _ => Card.CardType.None,
-        };
-    }
-
-    Card.CanPlayCondition ConvertToCondition(string condition)
-    {
-        return condition switch
-        {
-            "isGuard" => Card.CanPlayCondition.Guard,
-            "isWall" => Card.CanPlayCondition.Wall,
-            "isOccupied" => Card.CanPlayCondition.Occupied,
-            _ => Card.CanPlayCondition.None,
-        };
-    }
-
-    void AddMethodsToList(Card nextCopy, string divide, List<IEnumerator> list)
-    {
-        StringAndMethod dic = new StringAndMethod(nextCopy);
-
-        string[] methodsInStrings = divide.Split('/');
-        for (int k = 0; k < methodsInStrings.Length; k++)
-        {
-            if (dic.dictionary.TryGetValue(methodsInStrings[k], out IEnumerator method))
-                list.Add(method);
-            else
-                Debug.LogError($"\"{methodsInStrings[k]}\" isn't a method");
         }
     }
 }
